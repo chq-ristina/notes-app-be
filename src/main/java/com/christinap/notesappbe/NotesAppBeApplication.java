66f9f2dde96f2f0558eafe67e1cb7d807d9230da
Christina Phillips <christina.phillips9838@gmail.com>
2023-07-13 12:13:13 -0400

    Finished user authentication

---
+++ src/main/java/com/christinap/notesappbe/NotesAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.christinap.notesappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesAppBeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/christinap/notesappbe/user/Role.java
@@ -0,0 +1,6 @@
+package com.christinap.notesappbe.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
